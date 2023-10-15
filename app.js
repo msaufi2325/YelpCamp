defadd4e8e1d5979c03a06583b959fbb9648d804
Saufi <msaufi2325@gmail.com>
2023-10-16 00:11:46 +0900

    Modify app.js

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ const mongoSanitize = require("express-mongo-sanitize");
 
 const port = 3020;
 
-// const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL;
 // "mongodb://127.0.0.1:27017/yelp-camp"
-const dbUrl = "mongodb://127.0.0.1:27017/yelp-camp";
+// const dbUrl = "mongodb://127.0.0.1:27017/yelp-camp";
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
